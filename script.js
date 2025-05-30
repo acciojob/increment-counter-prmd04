34a4b7c443ffbf5ef91b6669956770a7c539a7e8
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-05-30 17:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let btn = document.getElementById("incrementBtn");
+
+btn.addEventListener("click", ()=>{
+	alert("button is clicked");
+	let count = document.getElementById("counter");
+	count.innerText = parseInt(count.innerText)+1;
+});
